5dbd909cdb1ebe79a0c38e065bbe5d9e7f5e4e25
alihanks <alihanks@gmail.com>
2015-07-28 10:54:31 -0700

    syntax style changes

--- offline/AnalysisTrain/Correlation/CorrelationFunctions.h
+++ offline/AnalysisTrain/Correlation/CorrelationFunctions.h
@@ -1,3 +1,4 @@
+// CorrelationFunction.h includes utility functions for Correlation code package
 #include <cmath>
 #include <iostream>
 #include <string>
@@ -22,9 +23,9 @@ const double EAST_HIGH_EDGE = 19*PI/16.0;
 inline float CalculateFoldedDphi(float phi1, float phi2)
 {
   float dphi = PHAngle(phi1-phi2);
-  if(dphi<0)    dphi+=2*PI;
-  if(dphi>2*PI) dphi-=2*PI;
-  if(dphi>PI)   dphi=2*PI-dphi;
+  if ( dphi < 0 )    dphi+=2*PI;
+  if ( dphi > 2*PI ) dphi-=2*PI;
+  if ( dphi > PI )   dphi=2*PI-dphi;
   return dphi;
 }
 
@@ -37,60 +38,55 @@ template<class T> void SetIso(T* trigger,
                               TH2F* h2_dR = NULL,
                               TH2F* h2_iso = NULL,
                               TH3F* h3_iso_acc = NULL
-)
+                              )
 {
   float pt = trigger->Pt();
   float phi = trigger->Phi();
   float eta = trigger->Eta();
 
   float etot = 0;
-  for(unsigned int iclus = 0; iclus < all_clus_vec.size(); iclus++){
+  for ( unsigned int iclus = 0; iclus < all_clus_vec.size(); iclus++ )
+  {
     float pt1 = all_clus_vec[iclus]->Pt();
-    if( pt1 < 0.2 ) continue;
+    if ( pt1 < 0.2 ) continue;
     float dR = all_clus_vec[iclus]->Angle(trigger->Vect());
-    //float deta = eta - all_clus_vec[iclus]->Eta();
-    float dphi = CalculateFoldedDphi(all_clus_vec[iclus]->Phi(),phi);
-    //dR = sqrt( dphi*dphi + deta*deta);
-    //if( dR < 0.0001 ) continue;
+    float dphi = CalculateFoldedDphi(all_clus_vec[iclus]->Phi(), phi);
+    // if ( dR < 0.0001 ) continue;
 
-    if( dR < Rcut ) etot += pt1;
-    
-    if( h3_dR ) h3_dR->Fill(pt,pt1,dR);
-    if( h3_etot ) h3_etot->Fill(pt,pt1/pt,dR);
-    if( h2_dR ) h2_dR->Fill(dR,dphi);
+    if ( dR < Rcut ) etot += pt1;
+
+    if ( h3_dR ) h3_dR->Fill(pt, pt1, dR);
+    if ( h3_etot ) h3_etot->Fill(pt, pt1/pt, dR);
+    if ( h2_dR ) h2_dR->Fill(dR, dphi);
   }
-  for(unsigned int itrk = 0; itrk < lessqualtrk_vec.size(); itrk++){
+
+  for ( unsigned int itrk = 0; itrk < lessqualtrk_vec.size(); itrk++ ) {
     float pt1 = lessqualtrk_vec[itrk]->Pt();
-    if( pt1 < 0.2 ) continue;
+    if ( pt1 < 0.2 ) continue;
     float dR = lessqualtrk_vec[itrk]->Angle(trigger->Vect());
-    //float deta = eta - lessqualtrk_vec[itrk]->Eta();
-    float dphi = CalculateFoldedDphi(lessqualtrk_vec[itrk]->Phi(),phi);
-    //dR = sqrt( dphi*dphi + deta*deta);
-    //if( dR < 0.001 ) continue;
+    float dphi = CalculateFoldedDphi(lessqualtrk_vec[itrk]->Phi(), phi);
 
-    if( dR < Rcut ) etot += pt1;
+    if ( dR < Rcut ) etot += pt1;
 
-    if( h3_dR ) h3_dR->Fill(pt,pt1,dR);
-    if( h3_etot ) h3_etot->Fill(pt,pt1/pt,dR);
-    if( h2_dR ) h2_dR->Fill(dR,dphi);
+    if ( h3_dR ) h3_dR->Fill(pt, pt1, dR);
+    if ( h3_etot ) h3_etot->Fill(pt, pt1/pt, dR);
+    if ( h2_dR ) h2_dR->Fill(dR, dphi);
   }
-  if( h2_iso ) h2_iso->Fill(etot/pt,pt);
-  // Trigger energy IS included in total here so check against 110% above cluster/pi0 energy
-  if( etot < 1.1*pt ) {
+  if ( h2_iso ) h2_iso->Fill(etot/pt, pt);
+  // Trigger energy IS included in total: check against 110%
+  if ( etot < 1.1*pt ) {
     trigger->SetIso(true);
-    if( h3_iso_acc ) h3_iso_acc->Fill(phi,eta,pt);
+    if ( h3_iso_acc ) h3_iso_acc->Fill(phi, eta, pt);
   }
   else trigger->SetIso(false);
   // Apply fiducial cut to photon before applying isolation
-  //float phphi = PHAngle(phi);
-  //if( (phphi > (WEST_LOW_EDGE + Rcut*0.1) && phphi < (WEST_HIGH_EDGE - Rcut*0.1)) || (phphi > (EAST_LOW_EDGE + Rcut*0.1) && phphi < (EAST_HIGH_EDGE - Rcut*0.1)) )
-  //if( !trigger->IsFiducial() )
-  //trigger->SetIso(false);
+  // if( !trigger->IsFiducial() )
+  // trigger->SetIso(false);
 }
 
 template<class T> void ClearVector(std::vector<T*> vec)
 {
-  for(unsigned int i=0; i < vec.size(); i++) delete vec[i];
+  for ( unsigned int i=0; i < vec.size(); i++ ) delete vec[i];
   vec.clear();
 }
 
