c495ced88e0952e6c33022e0822e6187e77c2a49
alihanks <alihanks@gmail.com>
2015-08-27 10:04:07 -0700

    bug fix

--- offline/analysis/Correlation/MakeJFs.C
+++ offline/analysis/Correlation/MakeJFs.C
@@ -216,7 +216,7 @@ MakeJFs::MakeJFs(int type, int centbin, int trigbin,
     norm_err = sqrt(norm_err);
     cout << "ZYAM norm err = " << CFinc->Integral(lbin,hbin) << "/(" << hbin << " - " << lbin << ") = " << norm_err << endl;
     for( int ib=1; ib<=CFflowZYAM->GetNbinsX(); ib++){
-      CFflowZYAM->SetBinError(norm_err);
+      CFflowZYAM->SetBinError(ib,norm_err);
     }
   }
 
