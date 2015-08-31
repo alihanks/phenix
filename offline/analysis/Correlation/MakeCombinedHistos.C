4a8dd601d6a0a75c43e5b791b27b0c6dd713f110
alihanks <alihanks@gmail.com>
2015-08-31 15:24:19 -0700

    all centralities

---
+++ offline/analysis/Correlation/MakeCombinedHistos.C
@@ -0,0 +1,18 @@
+#include "MakeCFs.h"
+#include "MakeJFs.h"
+
+#include <TFile.h>
+#include <TH1.h>
+#include <TH2.h>
+#include <TH3.h>
+#include <TF1.h>
+#include <TCanvas.h>
+#include <TLegend.h>
+#include <TLatex.h>
+#include <TVirtualPad.h>
+
+#include <string>
+#include <sstream>
+#include <iostream>
+
+using namespace std;
