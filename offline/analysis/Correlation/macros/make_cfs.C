d7fe1fa89b49b279215e201800a43e32aab402bb
alihanks <alihanks@gmail.com>
2015-11-12 13:33:02 -0800

    fixed naming inputs

--- offline/analysis/Correlation/macros/make_cfs.C
+++ offline/analysis/Correlation/macros/make_cfs.C
@@ -8,21 +8,24 @@ void make_cfs(int type = 0, int isiso = 0, const char* fin = "dAu_merged.root", 
   ostringstream trig_name, dphi_name, dphi_mix_name;
   if( type==0 ) {
   	trig_name << "h1_trig_pt_inc";
-  	dphi_name << "h3_dphi";
-  	dphi_mix_name << "h3_dphi_mix";
+  	if( isiso != 2 ) dphi_name << "h3_dphi";
+    else dphi_name << "h3_ptxidphi";
+  	dphi_mix_name << dphi_name.str() << "_mix";
   }
   if( type==1 ) {
   	trig_name << "h1_trig_pt_pi0";
-  	dphi_name << "h3_dphi_pi0";
-  	dphi_mix_name << "h3_dphi_pi0_mix";
+    if( isiso != 2 ) dphi_name << "h3_dphi_pi0";
+    else dphi_name << "h3_ptxidphi_pi0";
+  	dphi_mix_name << dphi_name.str() << "_mix";
   }
   if( type==2 ) {
   	trig_name << "h1_trig_pt_dec";
-  	dphi_name << "h2_dphi_dec";
-  	dphi_mix_name << "h2_dphi_dec_mix";
+    if( isiso != 2 ) dphi_name << "h2_dphi_dec";
+    else dphi_name << "h2_dphixi_dec";
+  	dphi_mix_name << dphi_name.str() << "_mix";
   }
 
-  if( isiso ) {
+  if( isiso==1 ) {
   	trig_name << "_iso";
   	dphi_name << "_iso";
   	dphi_mix_name << "_iso";
